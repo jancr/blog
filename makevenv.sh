daaaf6a15082359dde4a80d3ac5f4de2ac53c727
jcr <jcr@intomics.com>
2020-04-20 22:27:42 +0000

    no root requred

--- makevenv.sh
+++ makevenv.sh
@@ -1,12 +1,11 @@
 pyenv install 3.7.5
-mkvirtualenv blog
-workon blog
+pyenv virtualenv blog
 pip install -r requirements.txt
-cp blog.service /etc/systemd/system/blog.service
+sudo cp blog.service /etc/systemd/system/blog.service
 
 # setup nginx
-cp nginx_config/blog /etc/nginx/sites-available/blog
-ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled/
+sudo cp nginx_config/blog /etc/nginx/sites-available/blog
+sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled/
 
 
 
